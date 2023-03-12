b52e403a0c3fdf61436807fc046f92bb2cde7d56
li9520 <li.dp9520@gmail.com>
2023-03-12 20:45:28 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,15 @@ import * as React from 'react';
 
 import { createRoot } from 'react-dom/client';
 import 'regenerator-runtime';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import App from './App';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 if (module.hot) {
